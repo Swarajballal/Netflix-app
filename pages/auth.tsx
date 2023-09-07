bf3cee554983738facbf93fe6bad54e2ed912ab2
Swarajballal <swaraj.ballal@vit.edu.in>
2023-09-07 21:30:04 +0530

    Week 13

---
+++ pages/auth.tsx
@@ -0,0 +1,78 @@
+import Input from "../components/Input";
+import { useState , useCallback } from "react";
+
+const Auth = () => {
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [vairant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, [])
+
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="logo" className="h-12" />
+                </nav> 
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    <h2 className="text-white text-4xl mb-8 font-semibold">Sign in
+                    </h2>
+                    <div className="flex flex-col gap-4">
+
+                        <Input
+                        label="Username"
+                        onChange={(event: any) => {
+                                setName(event.target.value);
+                        }} 
+                        id="name"
+                        value={name}
+                        />
+
+                        <Input
+                        label="Email"
+                        onChange={(event: any) => {
+                                setEmail(event.target.value);
+                        }} 
+                        id="email"
+                        type="email"
+                        value={email}
+                        />
+
+                        <Input
+                        label="Pasword"
+                        onChange={(event: any) => {
+                                setPassword(event.target.value);
+                        }} 
+                        id="password"
+                        type="password"
+                        value={password}
+                        />
+
+                    </div>
+
+                    <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        Login
+                    </button>
+
+                    <p className="text-neutral-500 mt-12">
+                        First time using Netflix ?
+                        <span className="text-white ml-1 hover:underline cursor-pointer">
+                            Create an account
+                        </span>
+                    </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
+
