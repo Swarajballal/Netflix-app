f7a3e117f668b09bd85fe16ba377c37dce0a88d2
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-09-15 16:18:28 +0530

    Week 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
+import { NextPageContext } from 'next';
+import {signOut, getSession} from 'next-auth/react';
+import  useCurrentUser  from '@/hooks/useCurrentUser';
 
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {}
+  }
+}
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
-    <h1 className="text-green-500 text-2xl">Netflix Clone</h1>
+    <h1 className="text-green-500 text-4xl">Netflix Clone</h1>
+    <p
+    className='text-white'
+    >Logged in as : {user?.name}</p>
+    <button onClick={()=> {signOut()}}
+    className='h-10 w-full bg-white'
+    >Logout</button>
     </>
   )
 }
